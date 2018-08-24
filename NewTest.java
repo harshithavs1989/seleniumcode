df1f3317f7ac84555e45492eb714b6befd0a823b
Vinay <harshitha.vinay@philips.com>
2018-08-24 22:07:57 +0530

    Added if conditions

--- NewTest.java
+++ NewTest.java
@@ -5,9 +5,6 @@ public class NewTest {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Bye");
-		if(1==2)
-		{
-			System.out.println("I am hopeless in maths");
-		}
+		System.out.println("Where is conflict");
 	}
 }
